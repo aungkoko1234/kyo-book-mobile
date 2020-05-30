73ac7a6afca88151f94cf298a6a778e2a84288b1
paningyin <50397745+paningyin@users.noreply.github.com>
2020-05-30 21:07:10 +0630

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import register from './register';
+import login from './login';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
